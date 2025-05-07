a0aaee6e42b297b29d527c1269c46f0838b8a0c4
ABU HASIB MUHAMMAD NANZIL <106665861+Ahm-Nanzil@users.noreply.github.com>
2025-05-07 19:33:23 +0600

    cache modify

---
+++ ErpProject/bootstrap/cache/purchases_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Purchases\\Providers\\PurchasesServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Purchases\\Providers\\PurchasesServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
