0074c579b20a123fba55ae62d4f01838ad3a3582
ABU HASIB MUHAMMAD NANZIL <106665861+Ahm-Nanzil@users.noreply.github.com>
2025-06-07 17:24:47 +0600

    odified

--- assets/js/load.js
+++
@@ -1,9 +0,0 @@
-function hideShowContent() {
-  var loadingOverlay = document.getElementById('loading-overlay-site');
-  var siteContent = document.getElementById('mysite-content');
-
-  loadingOverlay.style.display = 'none';
-  siteContent.style.display = 'block';
-}
-
-window.addEventListener('load', hideShowContent);
