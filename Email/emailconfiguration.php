42f95ad6e889e9f54ee6d5588f2fb835d535ba45
ABU HASIB MUHAMMAD NANZIL <106665861+Ahm-Nanzil@users.noreply.github.com>
2025-07-01 22:32:21 +0600

    leads update

--- Email/emailconfiguration.php
+++ Email/emailconfiguration.php
@@ -6,7 +6,6 @@ return [
     'smtp_password' => 'hpitjdlzhhmnhurc',
     'from_email'    => 'ahmnanzil22334@gmail.com',
     'from_name'     => 'Webs',
-    'subject'       => 'Boost Your Online Presence with a Professional Website 🌐',
     'is_smtp'       => true,
     'smtp_secure'   => 'tls'
 ];
