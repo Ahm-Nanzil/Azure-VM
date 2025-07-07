834166a8e5d9b4c675b482ca34c84c539d66b898
ABU HASIB MUHAMMAD NANZIL <106665861+Ahm-Nanzil@users.noreply.github.com>
2025-07-07 14:44:18 +0600

    working mail

--- Email/emailconfiguration.php
+++ Email/emailconfiguration.php
@@ -3,7 +3,7 @@ return [
   'smtp_host' => 'smtp.gmail.com',
   'smtp_port' => 587,
   'smtp_username' => 'a194094@bscse.uiu.ac.bd',
-  'smtp_password' => 'hcpkqaykvqyzekkp',
+  'smtp_password' => 'pktwqghapovkenxp',
   'from_email' => 'a194094@bscse.uiu.ac.bd',
   'from_name' => 'Ahm Nanzil',
   'is_smtp' => true,
